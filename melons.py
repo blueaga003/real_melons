d7b204ff4b693c2639a8651c2ae1f3804ec3ed64
Hackbright Student <no-reply@hackbrightacademy.com>
2018-10-09 19:56:05 -0700

    Preliminary file

--- melons.py
+++ melons.py
@@ -1,25 +1,38 @@
 """Classes for melon orders."""
 
-
-class DomesticMelonOrder():
-    """A melon order within the USA."""
-
-    def __init__(self, species, qty):
+class AbstractMelonOrder():
+    """ An abstract base class that other Melon Orders inherit from."""
+    def __init__(self, species, qty, country_code=None):
         """Initialize melon order attributes."""
 
         self.species = species
         self.qty = qty
         self.shipped = False
+        # Test above line for DomesticMelonOrder
+        self.order_type = None
+        self.tax = None
+
+        def get_total(self):
+            """Calculate price, including tax."""
+
+            base_price = 5
+            total = (1 + self.tax) * self.qty * base_price
+
+            return total
+
+        def mark_shipped(self):
+            """Record the fact than an order has been shipped."""
+
+             self.shipped = True
+
+class DomesticMelonOrder():
+    """A melon order within the USA."""
+
+    def __init__(AbstractMelonOrder):
+        """Initialize melon order attributes."""
+
         self.order_type = "domestic"
         self.tax = 0.08
-
-    def get_total(self):
-        """Calculate price, including tax."""
-
-        base_price = 5
-        total = (1 + self.tax) * self.qty * base_price
-
-        return total
 
     def mark_shipped(self):
         """Record the fact than an order has been shipped."""
@@ -33,25 +46,8 @@ class InternationalMelonOrder():
     def __init__(self, species, qty, country_code):
         """Initialize melon order attributes."""
 
-        self.species = species
-        self.qty = qty
-        self.country_code = country_code
-        self.shipped = False
         self.order_type = "international"
         self.tax = 0.17
-
-    def get_total(self):
-        """Calculate price, including tax."""
-
-        base_price = 5
-        total = (1 + self.tax) * self.qty * base_price
-
-        return total
-
-    def mark_shipped(self):
-        """Record the fact than an order has been shipped."""
-
-        self.shipped = True
 
     def get_country_code(self):
         """Return the country code."""
